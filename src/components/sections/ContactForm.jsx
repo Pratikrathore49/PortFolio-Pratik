13ea4bbdb58c7678c97d638a295dd7e7022efcfd
Pratik Rathore <jipratik49@gmail.com>
2026-01-04 08:38:47 +0530

    "remove space"

--- src/components/sections/ContactForm.jsx
+++ src/components/sections/ContactForm.jsx
@@ -49,7 +49,7 @@ const ContactForm = () => {
         return;
       }
     const res =   await fetch("https://portfolio-backend-rqyl.onrender.com/api/contact", {
-        method: "POST",
+        method:"POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
       });
